d61a3e7f0055b482e5fc1d04b909f68adcda1c32
Agustin Treceno <agustin.treceno@gmail.com>
2016-07-26 00:27:10 +0100

    Redirect http requests to https

--- app.js
+++ app.js
@@ -13,9 +13,18 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.enable('trust proxy');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(fuction(req, res, next) {
+  if(req.secure) {
+    next();
+  } else {
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
